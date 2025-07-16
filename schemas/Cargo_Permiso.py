8d7a93ff8e9c2e3fb3d631730ac3af3ba805bc7a
Blasttechnology <Blasttechnology@outlook.com.ar>
2025-07-16 15:33:02 -0300

    Main

---
+++ schemas/Cargo_Permiso.py
@@ -0,0 +1,38 @@
+from pydantic import BaseModel
+from typing import List
+
+class CargoPermisoBase(BaseModel):
+    id_cargo: int
+    id_permiso: int
+
+class CargoPermisoCreate(CargoPermisoBase):
+    pass
+
+        
+class CargoPermisoResponse(CargoPermisoBase):
+    class Config:
+        from_attributes = True
+
+class PermisoSimple(BaseModel):
+    id_permiso: int
+    nombre: str
+    descripcion: str
+
+    class Config:
+        from_attributes = True
+
+# Para la respuesta del endpoint /permisos_por_cargo/{id_cargo}
+class PermisosDeCargoResponse(BaseModel):
+    permisos: List[PermisoSimple]
+
+    class Config:
+        from_attributes = True
+
+class CargoConPermisos(BaseModel):
+    id_cargo: int
+    cargo: str
+    descripcion: str
+    permisos: List[PermisoSimple]
+
+    class Config:
+        from_attributes = True
